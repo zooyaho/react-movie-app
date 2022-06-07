ac56fc12133c2ece86c1e7d16d02f20b48dce418
zooyaho <jiwoo1268@naver.com>
2022-06-07 12:50:42 +0900

    영화 관련 정보 화면에 출력

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ function App() {
 
   const getMovies = async () => {
     // 비동기 처리 async-await
-    const json = await (await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
-    )).json();
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+      )
+    ).json();
     // await은 Promise 객체를 리턴하는 부분 앞에만 붙일 수 있음.
-    // const json = await response.json(); 
+    // const json = await response.json();
     setMovies(json);
     setLoading(false);
   };
@@ -19,7 +21,7 @@ function App() {
   //   fetch(
   //     "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
   //   )
-      // .then((response) => response.json()) // 응답객체를 json형태로 변환, json을 리턴함.
+  // .then((response) => response.json()) // 응답객체를 json형태로 변환, json을 리턴함.
   //     .then((json) => { // 여기서 리턴한 json을 사용하는 것.
   //       setMovies(json.data.movies);
   //       setLoading(false);
@@ -30,7 +32,30 @@ function App() {
   }, []);
 
   console.log(movies);
-  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.data.movies.map((movie) => {
+            return (
+              <div key={movie.id}>
+                <img src={movie.medium_cover_image}/>
+                <h2>{movie.title}</h2>
+                <p>{movie.summary}</p>
+                <ul>
+                  {movie.genres.map((g) => {
+                    return <li key={g}>{g}</li>;
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
