6d10c490d1c97af8566f715b9598c849e3bb4d1d
zooyaho <jiwoo1268@naver.com>
2022-06-07 20:11:50 +0900

    Detail스타일 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,36 +1,55 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styles from "./Detail.module.css";
 
 const Detail = () => {
   const { id } = useParams();
-  const [movieInfo, setMovieInfo] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [movieInfo, setMovieInfo] = useState([]);
 
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     setMovieInfo(json);
+    setLoading(false);
   };
-  
+
   useEffect(() => {
     getMovie();
   }, []);
-  
+
   console.log(movieInfo);
-  // const {background_image_original,title, year, description_full, genres } = movieInfo.data.movie;
-  // console.log(background_image_original);
 
   return (
-    <div>
-      {/* <img src={background_image_original} alt={title} />
-      <h2>{title}</h2>
-      <p>{year}</p>
-      <p>{description_full}</p>
-      <ul>
-        {genres.map((g) => {
-          return <li key={g}>{g}</li>;
-        })}
-      </ul> */}
+    <div className={styles.container}>
+      {loading ? (
+        <p>Loaging...</p>
+      ) : (
+        <div className={styles.movie}>
+          <img
+            src={movieInfo.data.movie.background_image_original}
+            alt={movieInfo.data.movie.title}
+            className={styles.movie__img}
+          />
+          <div>
+            <h2 className={styles.movie__title}>
+              {movieInfo.data.movie.title}
+            </h2>
+            <h3 className={styles.movie__year}>{movieInfo.data.movie.year}</h3>
+            <p>
+              {movieInfo.data.movie.description_full.length > 235
+                ? `${movieInfo.data.movie.description_full.slice(0, 235)}...`
+                : movieInfo.data.movie.description_full}
+            </p>
+            <ul className={styles.movie__genres}>
+              {movieInfo.data.movie.genres.map((g) => (
+                <li key={g}>{g}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
